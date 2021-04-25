c583fa0a65603e0b99d7c21c1e31c7fcd4553636
Niab <niabwork2@gmail.com>
2021-04-25 16:40:31 +0700

    initial xcode project creation

---
+++ youtubeIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtubeIOS
+//
+//  Created by niab on Apr/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
