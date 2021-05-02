feca1dbf879896f46fd17134ac05e30c90dfd2d8
Niab <niabwork2@gmail.com>
2021-05-02 09:43:27 +0700

    add youtube api

---
+++ youtubeIOS/ConstantsTemplate.swift
@@ -0,0 +1,19 @@
+//
+//  ConstantsTemplate.swift
+//  youtubeIOS
+//
+//  Created by niab on May/02/21.
+//
+
+import Foundation
+
+/*
+struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL =
+        "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+}
+*/
---
+++ youtubeIOS/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtubeIOS
+//
+//  Created by niab on May/02/21.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
